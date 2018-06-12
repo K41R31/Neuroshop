b49c7f9b20257a38a3b0af08f55add4d3872d5d1
Tim Zech <zech.home@gmail.com>
2018-06-12 20:12:31 +0200

    ELM und LMA aktiviert

--- NeuralNet/NetStart.java
+++ NeuralNet/NetStart.java
@@ -7,6 +7,7 @@ package Neuroshop.NeuralNet;
 
 
 import Neuroshop.NeuralNet.learn.Backpropagation;
+import Neuroshop.NeuralNet.learn.ELM;
 import Neuroshop.NeuralNet.learn.LearningAlgorithm;
 import Neuroshop.NeuralNet.learn.LevenbergMarquardt;
 import Neuroshop.NeuralNet.math.IActivationFunction;
@@ -26,7 +27,7 @@ public class NetStart {
         DecimalFormat df = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
         df.setMaximumFractionDigits(20);
 
-        Double[][] _neuralDataSet = FileLoader.readDataFromFile("neuralnet/data/lilie.csv");
+        Double[][] _neuralDataSet = FileLoader.readDataFromFile("neuroshop/neuralnet/data/lilie.csv");
 
         int[] inputColumns = {0,1,2,3};
         int[] outputColumns = {4,5,6};
@@ -36,18 +37,17 @@ public class NetStart {
         int numberOfInputs = 4;
         int numberOfOutputs = 3;
         
-        int[] numberOfHiddenNeurons={3,3};
+        int[] numberOfHiddenNeurons={6};
         
         Linear outputAcFnc = new Linear(1.0);
         Sigmoid hdAcFnc = new Sigmoid(1.0);
-        Sigmoid h10AcFnc = new Sigmoid(1.0);
+//        Sigmoid h10AcFnc = new Sigmoid(1.0);
 
-        IActivationFunction[] hiddenAcFnc={hdAcFnc, h10AcFnc};
+        IActivationFunction[] hiddenAcFnc={hdAcFnc, /*h10AcFnc*/};
         
         NeuralNet nnlm = new NeuralNet(numberOfInputs,numberOfOutputs,numberOfHiddenNeurons,hiddenAcFnc,outputAcFnc);
         
-//        NeuralNet nnelm = new NeuralNet(numberOfInputs,numberOfOutputs
-//                ,numberOfHiddenNeurons,hiddenAcFnc,outputAcFnc);
+        NeuralNet nnelm = new NeuralNet(numberOfInputs,numberOfOutputs,numberOfHiddenNeurons,hiddenAcFnc,outputAcFnc);
         
         
         LevenbergMarquardt lma = new LevenbergMarquardt(nnlm,neuralDataSet,LearningAlgorithm.LearningMode.BATCH);
@@ -58,18 +58,18 @@ public class NetStart {
         lma.setMinOverallError(0.0001);
         lma.printTraining=true;
         
-//        ELM elm = new ELM(nnelm,neuralDataSet);
-//        elm.setGeneralErrorMeasurement(Backpropagation.ErrorMeasurement.SimpleError);
-//        elm.setOverallErrorMeasurement(Backpropagation.ErrorMeasurement.MSE);
-//        elm.setMinOverallError(0.0001);
-//        elm.printTraining=true;
+        ELM elm = new ELM(nnelm,neuralDataSet);
+        elm.setGeneralErrorMeasurement(Backpropagation.ErrorMeasurement.SimpleError);
+        elm.setOverallErrorMeasurement(Backpropagation.ErrorMeasurement.MSE);
+        elm.setMinOverallError(0.0001);
+        elm.printTraining=true;
         
         
         
         try{
             lma.forward();
             neuralDataSet.printNeuralOutput();
-            
+
             lma.train();
             System.out.println("End of training");
             if(lma.getMinOverallError()>=lma.getOverallGeneralError()){
@@ -84,36 +84,36 @@ public class NetStart {
                         +String.valueOf(lma.getMinOverallError()));
             System.out.println("Epochs of training:"
                         +String.valueOf(lma.getEpoch()));
-            
+
             System.out.println("Target Outputs:");
             neuralDataSet.printTargetOutput();
-            
+
             System.out.println("Neural Output after training:");
             lma.forward();
             neuralDataSet.printNeuralOutput();
             
-//            elm.forward();
+            elm.forward();
             neuralDataSet.printNeuralOutput();
-            
-//            elm.train();
-//            System.out.println("End of training");
-//            if(elm.getMinOverallError()>=elm.getOverallGeneralError()){
-//                System.out.println("Training successful!");
-//            }
-//            else{
-//                System.out.println("Training was unsuccessful");
-//            }
-//            System.out.println("Overall Error:"
-//                        +df.format(elm.getOverallGeneralError()));
-//            System.out.println("Min Overall Error:"
-//                        +df.format(elm.getMinOverallError()));
-//
-//            System.out.println("Target Outputs:");
-//            neuralDataSet.printTargetOutput();
-//
-//            System.out.println("Neural Output after training:");
-//            elm.forward();
-//            neuralDataSet.printNeuralOutput();
+
+            elm.train();
+            System.out.println("End of training");
+            if(elm.getMinOverallError()>=elm.getOverallGeneralError()){
+                System.out.println("Training successful!");
+            }
+            else{
+                System.out.println("Training was unsuccessful");
+            }
+            System.out.println("Overall Error:"
+                        +df.format(elm.getOverallGeneralError()));
+            System.out.println("Min Overall Error:"
+                        +df.format(elm.getMinOverallError()));
+
+            System.out.println("Target Outputs:");
+            neuralDataSet.printTargetOutput();
+
+            System.out.println("Neural Output after training:");
+            elm.forward();
+            neuralDataSet.printNeuralOutput();
             
 
         }
