f9b9385c24ff6fc933cf4410f34513601a110942
Tim Zech <zech.home@gmail.com>
2018-06-26 22:50:22 +0200

    kwt

--- NetzTest.java
+++ NetzTest.java
@@ -3,7 +3,9 @@ package Neuroshop;
 import Neuroshop.ANN.ANNLearn;
 
 public class NetzTest {
-    //TODO: BITTE IGNORIEREN! Content kommt noch!
+
+
+
     public static void main (String [] args) {
         ANNLearn test = new ANNLearn();
 //        test.train();
