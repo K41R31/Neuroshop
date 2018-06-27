3c3c9bdc5b83fc812cffbf68543f931e77b4da1f
Tim Zech <zech.home@gmail.com>
2018-06-27 18:49:45 +0200

    Netz zum Laufen gebracht

---
+++ DiagnosisExample.java
@@ -0,0 +1,219 @@
+package Neuroshop;
+
+
+
+import Neuroshop.ANN.Data.DataNormalization;
+import Neuroshop.ANN.Data.DataSet;
+import Neuroshop.ANN.Data.NeuralDataSet;
+import Neuroshop.ANN.Init.UniformInitialization;
+import Neuroshop.ANN.Learn.Backpropagation;
+import Neuroshop.ANN.Learn.LearningAlgorithm;
+import Neuroshop.ANN.Math.*;
+import Neuroshop.ANN.Neural.NeuralException;
+import Neuroshop.ANN.Neural.NeuralNet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * DiagnosisExample This class performs use cases described in
+ * chapter 6 of the book: breast cancer and diabetes data. Data are loaded, data
+ * normalization is done, neural net is created using parameters defined,
+ * Backpropagation algorithm is used to make neural net learn and charts are
+ * plotted
+ * 
+ * @authors Alan de Souza, Fábio Soares
+ * @version 0.1
+ * 
+ */
+public class DiagnosisExample {
+
+	public static void main(String[] args) {
+
+		RandomNumberGenerator.setSeed(7);
+		
+		String CHOSEN_OPTION = " ";
+		int numberOfInputs  = 0;
+		int numberOfOutputs = 0;
+		int[] inputColumns  = null;
+		int[] outputColumns = null;
+		
+		DataSet dataSet = new DataSet();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int experiment = 0;
+		
+		while(true) {
+			boolean flagNeural = true;
+			
+			experiment++;
+			System.out.println("*** EXPERIMENT #"+experiment+" ***");
+			
+			System.out.println("Number of epochs:");
+			int typedEpochs = sc.nextInt();
+			
+			System.out.println("Number of neurons in hidden layer:");
+			int typedNumHdnLayer = sc.nextInt();
+			
+			System.out.println("Learning rate:");
+			double typedLearningRate = sc.nextDouble();
+			
+			System.out.println("Which dataset would you like to use?");
+			System.out.println("1) Lilien");
+			System.out.println("2) Diabetes");
+			System.out.println("99) Quit");
+			int option = sc.nextInt();
+			
+			switch (option) {
+				case 1:
+					CHOSEN_OPTION   = "Lilien";
+					numberOfInputs  = 4;
+					numberOfOutputs = 1;
+					
+					// load data
+					dataSet = new DataSet("Neuroshop/Ressources/Data", "new_data.txt");
+					inputColumns  = new int[] {0, 1, 2, 3};
+					outputColumns = new int[] {4};
+					
+					break;
+				case 2:
+					CHOSEN_OPTION   = "Diabetes";
+					numberOfInputs  = 8;
+					numberOfOutputs = 2;
+					
+					// load data
+					dataSet = new DataSet("data", "diabetes_fulldata.txt");
+					inputColumns  = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+					outputColumns = new int[] { 8, 9 };
+					
+					break;
+				case 99:
+					System.out.println("Successful exit!");
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Invalid option...");
+					flagNeural = false;
+			}
+
+			if(flagNeural) {
+			
+				int[] numberOfHiddenNeurons = { typedNumHdnLayer };
+
+				Sigmoid hl0Fnc = new Sigmoid(1.0);
+				Linear outputAcFnc = new Linear(1.0);
+			
+				IActivationFunction[] hiddenAcFnc = { hl0Fnc };
+			
+				System.out.println("Creating Neural Network...");
+				NeuralNet nn = new NeuralNet(numberOfInputs, numberOfOutputs, numberOfHiddenNeurons, hiddenAcFnc, outputAcFnc, new UniformInitialization(-1.0, 1.0));
+				System.out.println("Neural Network created!");
+				//nn.print();
+				
+				// load data
+				double[][] _neuralDataSet = dataSet.getData();
+			
+				// normalize data
+				DataNormalization dn = new DataNormalization(-1.0, 1.0);
+				double[][] dataNormalized = new double[_neuralDataSet.length][_neuralDataSet[0].length];
+				dataNormalized = dn.normalize(_neuralDataSet);
+				
+				double[][] dataNormalizedToTrain = Arrays.copyOfRange(dataNormalized, 0, (int) Math.ceil(dataNormalized.length*(0.8)));
+				double[][] dataNormalizedToTest  = Arrays.copyOfRange(dataNormalized, (int) Math.ceil(dataNormalized.length*(0.8))+1, dataNormalized.length);
+				
+				// normalized data to train ANN:
+				NeuralDataSet neuralDataSetToTrain = new NeuralDataSet(dataNormalizedToTrain, inputColumns, outputColumns);
+				
+				// normalized data to test ANN:
+				NeuralDataSet neuralDataSetToTest = new NeuralDataSet(dataNormalizedToTest, inputColumns, outputColumns);
+
+				System.out.println("Getting the first output of the neural network");
+			
+				// create ANN and define parameters to TRAIN: 
+				Backpropagation backprop = new Backpropagation(nn, neuralDataSetToTrain, LearningAlgorithm.LearningMode.BATCH);
+				backprop.setLearningRate( typedLearningRate );
+				backprop.setMaxEpochs( typedEpochs );
+				backprop.setGeneralErrorMeasurement(Backpropagation.ErrorMeasurement.SimpleError);
+				backprop.setOverallErrorMeasurement(Backpropagation.ErrorMeasurement.MSE);
+				backprop.setMinOverallError(0.001);
+				backprop.setMomentumRate(0.7);
+				backprop.setTestingDataSet(neuralDataSetToTest);
+				backprop.printTraining = true;
+				backprop.showPlotError=true;
+				
+				// train ANN:
+				try {
+					backprop.forward();
+					//neuralDataSetToTrain.printNeuralOutput();
+					backprop.train();
+					System.out.println("End of training");
+					if (backprop.getMinOverallError() >= backprop.getOverallGeneralError()) {
+						System.out.println("Training successful!");
+					} else {
+						System.out.println("Training was unsuccessful");
+					}
+					System.out.println("Overall Error:" + String.valueOf(backprop.getOverallGeneralError()));
+					System.out.println("Min Overall Error:" + String.valueOf(backprop.getMinOverallError()));
+					System.out.println("Epochs of training:" + String.valueOf(backprop.getEpoch()));
+
+					backprop.showErrorEvolution();
+
+					System.out.println("Target Outputs (TRAIN):");
+					neuralDataSetToTrain.printTargetOutput();
+			
+					System.out.println("Neural Output after training:");
+					backprop.forward();
+					neuralDataSetToTrain.printNeuralOutput();
+				} catch (NeuralException ne) {
+					ne.printStackTrace();
+				}
+			
+				//System.out.println("Dataset to test created");
+				//neuralDataSetToTest.printInput();
+				//neuralDataSetToTest.printTargetOutput();
+
+		
+			} //end if flagNeural
+			
+		} //end while loop 
+
+	}
+
+	private static double[][] extractMatrixByArrayList(int[] outputColumns, double[][] data, ArrayList<ArrayList<Double>> list) {
+		double[][] matrix = new double[data.length][outputColumns.length];
+		int i = 0, j = 0;
+		for (ArrayList<Double> arrayList : list) {
+			for (Double value : arrayList) {
+				matrix[i][j] = value;
+				j++;
+			}
+			i++;
+			j=0;
+		}
+		return matrix;
+	}
+
+	private static double[][] adaptData(int[] outputColumns, double[][] data,
+			double[] dataOutput1, double[] dataOutput2) {
+		double[][] matrix = new double[data.length][outputColumns.length];
+		for (int k = 0; k < dataOutput1.length; k++) {
+			double v1 = dataOutput1[k];
+			double v2 = dataOutput2[k];
+			if(v1 <= 0.50) {
+				matrix[k][0] = 0.0;
+			}else {
+				matrix[k][0] = 1.0;
+			}
+			if(v2 <= 0.50) {
+				matrix[k][1] = 0.0;
+			}else {
+				matrix[k][1] = 1.0;
+			}
+		}
+		return matrix;
+	}
+
+}
--- Prototyp.java
+++ Prototyp.java
@@ -4,35 +4,34 @@ import Neuroshop.ANN.ANNLearn;
 import Neuroshop.ANN.Data.DataNormalization;
 import Neuroshop.ANN.Data.DataSet;
 import Neuroshop.ANN.Learn.LearningAlgorithm;
+import Neuroshop.ANN.Math.HyperTan;
 import Neuroshop.ANN.Math.IActivationFunction;
 import Neuroshop.ANN.Math.Linear;
 import Neuroshop.ANN.Math.Sigmoid;
 
-
-
 public class Prototyp {
 
     public static void main (String[] args)  {
-        new Thread();
 
-        int x = 5;
-        int[] numberOfHiddenNeurons = {x};
-        double learningRate = 0.001;
-        int maxEpochs = 500;
-        double momentumRate = 0.7;
-        double minOverallError = 0.001;
+        int[] numberOfHiddenNeurons = {3,2};
+        double learningRate = (0.01);
+        int maxEpochs = (15000);
+        double momentumRate = (0.7);
+        double minOverallError = (0.01);
         int[] inputColumns = {0, 1, 2, 3};
         int[] outputColumns = {4};
-        double dataPercentage = 0.8;
+        double dataPercentage = (0.8);
 
         Sigmoid h10Fnc = new Sigmoid(1.0);
-        Linear outputActFnc = new Linear (1.0);
-        IActivationFunction[] actFnc = {h10Fnc};
+        HyperTan h20Fnc = new HyperTan();
+
+        IActivationFunction outputActFnc = new Linear (1.0);
+        IActivationFunction[] actFnc = {h10Fnc ,h20Fnc};
 
         LearningAlgorithm.LearningMode lMode = LearningAlgorithm.LearningMode.BATCH;
         DataNormalization dataNormType = new DataNormalization(-1.0, 1.0);
 
-        DataSet dataSet = new DataSet("Neuroshop/Ressources/Data", "new_data.txt");
+        DataSet dataSet = new DataSet("Neuroshop/Ressources/Data", "new_data.csv");
         ANNLearn aL = new ANNLearn();
         aL.train(dataSet, inputColumns, outputColumns, dataPercentage, maxEpochs, numberOfHiddenNeurons, minOverallError, learningRate, momentumRate, actFnc, outputActFnc, lMode, dataNormType);
 
