853ab0d3d0c55a3aff6be9d44e6cb861996ace52
Kai <kai.rei@web.de>
2018-06-28 20:10:28 +0200

    pull

---
+++ ANN/InitANN.java
@@ -0,0 +1,48 @@
+package Neuroshop.ANN;
+
+import Neuroshop.ANN.Data.DataNormalization;
+import Neuroshop.ANN.Data.DataSet;
+import Neuroshop.ANN.Learn.LearningAlgorithm;
+import Neuroshop.ANN.Math.IActivationFunction;
+import Neuroshop.ANN.Math.Linear;
+import Neuroshop.ANN.Math.Sigmoid;
+import Neuroshop.Models.ANNModel;
+
+public class InitANN {
+
+    private static ANNModel annModel;
+
+    public InitANN() {
+
+        annModel = new ANNModel();
+
+        int[] numberOfHiddenNeurons = {6};
+
+        double learningRate = (1);
+        int maxEpochs = (5000);
+        double momentumRate = (0.7);
+        double minOverallError = (0.007);
+        int[] inputColumns = {0, 1, 2, 3};
+        int[] outputColumns = {4};
+        double dataPercentage = (0.8);
+
+        Sigmoid h10Fnc = new Sigmoid(1.0);
+    //        HyperTan h20Fnc = new HyperTan(2.0);
+
+        IActivationFunction outputActFnc = new Linear(1.0);
+        IActivationFunction[] actFnc = {h10Fnc};
+
+
+        LearningAlgorithm.LearningMode lMode = LearningAlgorithm.LearningMode.BATCH;
+        DataNormalization dataNormType = new DataNormalization(0, 1.0);
+
+        DataSet dataSet = new DataSet("Neuroshop\\Ressources\\Data", "new_data.txt" ); // Spalten müssen mit "," getrennt werden
+
+            annModel.setDataSet(dataSet.getData());
+        ANNLearn aL = new ANNLearn();
+        //Init Model----------------------------------------------------------------------------------------------------
+            aL.initModel(annModel);
+
+            aL.train(dataSet, inputColumns, outputColumns, dataPercentage, maxEpochs, numberOfHiddenNeurons, minOverallError, learningRate, momentumRate, actFnc, outputActFnc, lMode, dataNormType);
+    }
+}
--- Models/ANNModel.java
+++ Models/ANNModel.java
@@ -1,26 +1,37 @@
 package Neuroshop.Models;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Observable;
 
 public class ANNModel extends Observable {
 
+    private File dataSetFile;
     private ArrayList<ArrayList<ArrayList<Double>>> newWeights;
     private double[][] dataSet;
 
-    public ArrayList<ArrayList<ArrayList<Double>>> getNewWeights() {
-        return newWeights;
-    }
 
     public void setNewWeights(ArrayList<ArrayList<ArrayList<Double>>> newWeights) {
         this.newWeights = newWeights;
     }
 
-    public double[][] getDataSet() {
-        return dataSet;
+    public ArrayList<ArrayList<ArrayList<Double>>> getNewWeights() {
+        return newWeights;
+    }
+
+    public void setRawDataSetFile(File dataSetFile) {
+        this.dataSetFile = dataSetFile;
+    }
+
+    public File getRawDataSetFile() {
+        return dataSetFile;
     }
 
     public void setDataSet(double[][] dataSet) {
         this.dataSet = dataSet;
+    }
+
+    public double[][] getDataSet() {
+        return dataSet;
     }
 }
