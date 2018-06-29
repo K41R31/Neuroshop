d8da9b218620940f46873c2d928ef3f1594e4b78
Dominik <d.preusser1995@web.de>
2018-06-29 05:02:08 +0200

    ID für Logik Tutorial

--- Tutorial/TutorialController.java
+++ Tutorial/TutorialController.java
@@ -1,4 +1,10 @@
 package Neuroshop.Tutorial;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class TutorialController {
+
+    @FXML
+    AnchorPane tutorialStart,dataPrep,importData,dataSettings,dragNeural,neuralSettings,dropDiagram,changeValues,startNetwork;
 }
