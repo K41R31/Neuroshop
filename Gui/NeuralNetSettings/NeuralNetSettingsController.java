d8400b820e930fa63940218496ed34ce06b8fcce
Kai <kai.rei@web.de>
2018-07-04 12:45:26 +0200

    Tut

--- Gui/NeuralNetSettings/NeuralNetSettingsController.java
+++ Gui/NeuralNetSettings/NeuralNetSettingsController.java
@@ -169,8 +169,9 @@ public class NeuralNetSettingsController implements Observer {
     private void updateEpochPane() {
         Timeline updateEpochPane = new Timeline();
         updateEpochPane.getKeyFrames().addAll(
-                new KeyFrame(new Duration(100), event -> actualEpoch.setText(String.valueOf(annModel.getActualEpoch()))));
-                new KeyFrame(new Duration(100), event -> actualOverallError.setText(String.valueOf(annModel.getOverallError())));
+                new KeyFrame(new Duration(100), event -> actualEpoch.setText(String.valueOf(annModel.getActualEpoch()))),
+                new KeyFrame(new Duration(100), event -> actualOverallError.setText(String.valueOf(annModel.getOverallError())))
+        );
         updateEpochPane.setCycleCount(Animation.INDEFINITE);
         updateEpochPane.play();
     }
